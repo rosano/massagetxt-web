370452963abd2de012eed665802aecbe576aa15f
Rosano Coutinho <pub@xeari.com>
2019-11-14 12:11:42 -0500

    Style trace item

--- os-app/dev-explorer/submodules/MSTExplorerTraceItem/main.svelte
+++ os-app/dev-explorer/submodules/MSTExplorerTraceItem/main.svelte
@@ -7,3 +7,12 @@ export let MSTExplorerTraceItemOperation = '';
 <div class="MSTExplorerTraceItemOperation">{ MSTExplorerTraceItemOperation }</div>
 
 </div>
+
+<style>
+.MSTExplorerTraceItem {
+	padding: 5px 0;
+	border-top: 1px solid rgba(0, 0, 0, 0.1);
+
+	font-family: monospace;
+}
+</style>
