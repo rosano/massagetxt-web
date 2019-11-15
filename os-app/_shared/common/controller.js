39b38c8ede1ad041ffc4a05adcfe9c36e5a04b91
Rosano Coutinho <pub@xeari.com>
2019-11-15 09:19:48 -0500

    Define MSWSharedGitHubLinkGuardMiddleware

--- os-app/_shared/common/controller.js
+++ os-app/_shared/common/controller.js
@@ -1,4 +1,10 @@
-//_ OLSKControllerSharedStaticAssetFolders
+exports.OLSKControllerSharedMiddlewares = function() {
+	return {
+		MSWSharedGitHubLinkGuardMiddleware (req, res, next) {
+			return next(require('./logic.js').MSWSharedGitHubLinkGuard(process.env))
+		},
+	};
+};
 
 exports.OLSKControllerSharedStaticAssetFolders = function() {
 	return [
