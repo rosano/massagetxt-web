e90cc25dbb0ee27ba942e4da69b01f3eb5f1d7b5
Rosano Coutinho <pub@xeari.com>
2019-11-15 10:16:11 -0500

    Map short params to props

--- os-app/open-explore/ui-behaviour.js
+++ os-app/open-explore/ui-behaviour.js
@@ -5,7 +5,9 @@ const mod = {
 	SetupEverything() {
 		const app = new Main({
 			target: document.getElementById('MSWPromptTarget'),
-			props: Object.fromEntries((new window.URLSearchParams(window.location.search)).entries()),
+			props: Object.fromEntries(Array.from((new window.URLSearchParams(window.location.search)).entries()).map(function ([key, value]) {
+				return [key === 'raw' ? 'MSWPromptRaw' : (key === 'massage' ? 'MSWPromptMassage' : key), value];
+			})),
 		});
 	},
 
