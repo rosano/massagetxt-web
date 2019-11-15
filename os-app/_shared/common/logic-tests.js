54899d251d0fa590f000fb2d8d182ffe00fb1ea1
Rosano Coutinho <pub@xeari.com>
2019-11-15 09:19:41 -0500

    Spec MSWSharedGitHubLinkGuard

---
+++ os-app/_shared/common/logic-tests.js
@@ -0,0 +1,31 @@
+const { throws, deepEqual } = require('assert');
+
+const mainModule = require('./logic.js');
+
+describe('MSWSharedGitHubLinkGuard', function testMSWSharedGitHubLinkGuard() {
+
+	const StubEnvValid = function () {
+		return {
+			MSW_SHARED_GITHUB_URL: 'alfa',
+		};
+	};
+
+	it('throws if not object', function() {
+		throws(function() {
+			mainModule.MSWSharedGitHubLinkGuard(null);
+		}, /MSWErrorInputNotValid/);
+	});
+
+	it('returns error if no MSW_SHARED_GITHUB_URL', function () {
+		deepEqual(mainModule.MSWSharedGitHubLinkGuard(Object.assign(StubEnvValid(), {
+			MSW_SHARED_GITHUB_URL: null,
+		})), new Error('MSW_SHARED_GITHUB_URL not defined'));
+	});
+
+	it('returns error if MSW_SHARED_GITHUB_URL blank', function () {
+		deepEqual(mainModule.MSWSharedGitHubLinkGuard(Object.assign(StubEnvValid(), {
+			MSW_SHARED_GITHUB_URL: ' ',
+		})), new Error('MSW_SHARED_GITHUB_URL not defined'));
+	});
+
+});
