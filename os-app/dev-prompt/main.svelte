054f1278b7865c548b7ad7f0131fcbfd270fa5c1
Rosano Coutinho <pub@xeari.com>
2019-11-16 12:06:19 -0500

    Exclude function arguments

--- os-app/dev-prompt/main.svelte
+++ os-app/dev-prompt/main.svelte
@@ -105,7 +105,7 @@ import MSWPromptTraceItem from './submodules/MSWPromptTraceItem/main.svelte';
 
 		<div class="MSWPromptTrace">
 			{#each mod._ValueTraceObjects as item}
-				<MSWPromptTraceItem MSWPromptTraceItemOperation={ item.MSTTraceOperation } MSWPromptTraceItemInput={ MSWPromptLogic.MSWStringify(MSWPromptLogic.MSWVisualString(item.MSTTraceInput)) } MSWPromptTraceItemArguments={ (item.MSTTraceArguments || []).map(function (e) {
+				<MSWPromptTraceItem MSWPromptTraceItemOperation={ item.MSTTraceOperation } MSWPromptTraceItemInput={ MSWPromptLogic.MSWStringify(MSWPromptLogic.MSWVisualString(item.MSTTraceInput)) } MSWPromptTraceItemArguments={ (item.MSTTraceArguments || []).filter(function (e) { return typeof e !== 'function'; }).map(function (e) {
 					return MSWPromptLogic.MSWStringify(MSWPromptLogic.MSWVisualString(e))
 				}) } OLSKLocalized={ OLSKLocalized } />
 			{/each}
