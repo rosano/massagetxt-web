7911bed836434120752a89c6a07d61301dabbe3b
Rosano Coutinho <pub@xeari.com>
2020-07-11 10:10:06 -0300

    Purge OLSKTestingCanonicalFor

--- os-app/_shared/common/ui-test-_access.js
+++ os-app/_shared/common/ui-test-_access.js
@@ -3,7 +3,7 @@ import { deepEqual } from 'assert';
 describe('MSTCommon_Access', function test_MSTCommon_Access () {
 
 	it('redirects MSTCommonIdentityRedirect', async function() {
-		deepEqual((await (await browser.fetch('http://loc.tests' + OLSKTestingCanonicalFor(require('./controller.js').OLSKControllerRoutes().MSTCommonIdentityRedirect.OLSKRoutePath))).text()).slice(0, 10), '<?xml vers')
+		deepEqual((await (await browser.fetch('http://loc.tests' + OLSKTestingCanonical(require('./controller.js').OLSKControllerRoutes().MSTCommonIdentityRedirect))).text()).slice(0, 10), '<?xml vers')
 	});
 
 });
