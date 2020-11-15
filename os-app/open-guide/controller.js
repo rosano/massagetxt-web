dc08de75046fa9d0d881081a23335373ce19eaf9
Rosano Coutinho <pub@xeari.com>
2020-11-15 18:22:03 -0300

    Extract OLSKMarkdownContent

--- os-app/open-guide/controller.js
+++ os-app/open-guide/controller.js
@@ -4,11 +4,7 @@ exports.OLSKControllerUseLivereload = function() {
 
 function OLSKRouteFunction (req, res, next) {
 	return res.OLSKLayoutRender(require('path').join(__dirname, 'ui-view'), {
-		MSWGuideContent: require('marked').setOptions({
-			gfm: true,
-			headerIds: false,
-		})(require('OLSKString').OLSKStringReplaceTokens(
-			require('fs').readFileSync(require('path').join(__dirname, `text.${ res.locals.OLSKSharedPageCurrentLanguage }.md`), 'utf-8'), {})),
+		MSWGuideContent: res.OLSKMarkdownContent(require('path').join(__dirname, `text.${ res.locals.OLSKSharedPageCurrentLanguage }.md`)),
 		OLSKStringReplaceTokens: require('OLSKString').OLSKStringReplaceTokens,
 	});
 };
--- os-app/open-vitrine/controller.js
+++ os-app/open-vitrine/controller.js
@@ -9,12 +9,10 @@ exports.OLSKControllerRoutes = function() {
 		OLSKRouteSignature: 'MSWVitrineRoute',
 		OLSKRouteFunction (req, res, next) {
 			return res.OLSKLayoutRender(require('path').join(__dirname, 'ui-view'), {
-				MSWVitrineContent: require('OLSKString').OLSKStringReplaceTokens(require('marked').setOptions({
-					gfm: true,
-					headerIds: false,
-				})(require('fs').readFileSync(require('path').join(__dirname, `text.${ res.locals.OLSKSharedPageCurrentLanguage }.md`), 'utf-8')), {
+				MSWVitrineContent: res.OLSKMarkdownContent(require('path').join(__dirname, `text.${ res.locals.OLSKSharedPageCurrentLanguage }.md`), {
 					MSWVitrineTokenGuideURL: res.locals.OLSKCanonicalFor('MSWGuideRoute'),
 					MSWVitrineTokenExploreURL: res.locals.OLSKCanonicalFor('MSWExploreRoute'),
+
 					MSW_SHARED_GITHUB_URL: process.env.MSW_SHARED_GITHUB_URL,
 				}),
 				OLSKStringReplaceTokens: require('OLSKString').OLSKStringReplaceTokens,
