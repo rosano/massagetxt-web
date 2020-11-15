5877b0291c02489578ad1913566ba386f471d0ed
Rosano Coutinho <pub@xeari.com>
2020-11-15 13:37:40 -0300

    Extract OLSKPostinstallHotfix

--- package-postinstall.js
+++ package-postinstall.js
@@ -1,18 +1,18 @@
-(function OLSKPostinstallPatchZombieForUITests() {
+(function OLSKPostinstallHotfix() {
 	if (process.env.NODE_ENV === 'production') {
 		return;
 	}
 
-	let filePath = './node_modules/zombie/lib/document.js';
-
-	require('fs').writeFileSync(filePath, require('OLSKString').OLSKStringPatch(
-		require('fs').readFileSync(filePath, 'utf8'),
-		'this.dispatchEvent(event);',
-		`this.dispatchEvent(event)
-			const handled = browser.emit('OLSKMessage', data);
-		  if (!handled)
-		      browser.log('Unhandled message("%s")');`
-	));
+	Object.entries(require('OLSKHotfix').OLSKHotfixPatches()).forEach(function ([path, patches]) {
+		if (!require('fs').existsSync(path)) {
+			return;
+		}
+		
+		Object.entries(patches).forEach(function ([search, replace]) {
+			require('fs').writeFileSync(path, require('OLSKString').OLSKStringPatch(
+				require('fs').readFileSync(path, 'utf8'), search, replace));
+		});
+	});
 })();
 
 (function OLSKPostinstallExternalAssets() {
