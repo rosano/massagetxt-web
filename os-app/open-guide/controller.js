064605f3c88d54999ca954262b85dc6bec4e1b69
Rosano Coutinho <pub@xeari.com>
2021-04-27 10:13:59 -0300

    Move livereload to OLSKExpress

--- os-app/open-guide/controller.js
+++ os-app/open-guide/controller.js
@@ -1,7 +1,3 @@
-exports.OLSKControllerUseLivereload = function() {
-	return process.env.NODE_ENV === 'development';
-};
-
 function OLSKRouteFunction (req, res, next) {
 	return res.OLSKExpressLayoutRender(require('path').join(__dirname, 'ui-view'), {
 		MSWGuideContent: res.OLSKMarkdownContent(require('path').join(__dirname, `text.${ res.locals.OLSKSharedPageCurrentLanguage }.md`)),
