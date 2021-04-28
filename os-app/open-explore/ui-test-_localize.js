212ad3ba70cb2358b0c1f2a5a9d9c12e3d373859
Rosano Coutinho <pub@xeari.com>
2021-04-28 10:25:04 -0300

    Tidy tests

--- os-app/open-explore/ui-test-_localize.js
+++ os-app/open-explore/ui-test-_localize.js
@@ -6,7 +6,7 @@ const uLocalized = function (inputData) {
 	return OLSKTestingLocalized(inputData, OLSKRoutingLanguage);
 };
 
-describe(`MSWExplore_Localize-${ OLSKRoutingLanguage }`, function () {
+describe('MSWExplore_Localize-' + OLSKRoutingLanguage, function () {
 
 	before(function() {
 		return browser.OLSKVisit(kDefaultRoute, {
--- os-app/open-vitrine/ui-test-_localize.js
+++ os-app/open-vitrine/ui-test-_localize.js
@@ -6,7 +6,7 @@ kDefaultRoute.OLSKRouteLanguageCodes.forEach(function (OLSKRoutingLanguage) {
 		return OLSKTestingLocalized(inputData, OLSKRoutingLanguage);
 	};
 
-	describe(`MSWVitrine_Localize-${ OLSKRoutingLanguage }`, function () {
+	describe('MSWVitrine_Localize-' + OLSKRoutingLanguage, function () {
 
 		before(function() {
 			return browser.OLSKVisit(kDefaultRoute, {
